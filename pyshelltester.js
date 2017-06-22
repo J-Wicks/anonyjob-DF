7e8ed8d29e94855b03475d7ab501deb0fcb6245c
andrew.atreides@gmail.com <andrew.atreides@gmail.com>
2017-06-22 17:04:50 -0400

    Some simple node to python scripts.

--- pyshelltester.js
+++ pyshelltester.js
@@ -1,6 +1,20 @@
 const PythonShell = require('python-shell')
-const pyTester = new PythonShell('pyTester.py')
+const axios = require('axios')
 
-pyTester.on('message', (message) => {
-  console.log(message)
+
+const pyTester = new PythonShell('pyTester.py', {mode: 'json'})
+
+let output = ''
+pyTester.stdout.on('data', data => {
+  output += '' + data
 })
+
+axios.get('https://pacific-falls-40871.herokuapp.com/api/allData')
+.then(res => res.data)
+.then(data => {
+  pyTester.send(data).end(err => {
+    if (err) console.error(err)
+    console.log(output)
+  })
+})
+.catch(console.error)
