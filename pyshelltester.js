86bbe92628eee53941384663aacecb02ba2e29b2
andrew.atreides@gmail.com <andrew.atreides@gmail.com>
2017-06-23 13:16:06 -0400

    Created basis for creating new folders based on existing data.

--- pyshelltester.js
+++ pyshelltester.js
@@ -1,5 +1,6 @@
 const PythonShell = require('python-shell')
 const axios = require('axios')
+const fs = require('fs')
 
 
 const pyTester = new PythonShell('pyTester.py', {mode: 'json'})
@@ -12,9 +13,19 @@ pyTester.stdout.on('data', data => {
 axios.get('https://pacific-falls-40871.herokuapp.com/api/allData')
 .then(res => res.data)
 .then(data => {
-  pyTester.send(data).end(err => {
-    if (err) console.error(err)
-    console.log(output)
+  // pyTester.send(data).end(err => {
+  //   if (err) console.error(err)
+  //   console.log(output)
+  data.forEach((user, idx) => {
+    if (!fs.existsSync(__dirname + `/Categories/Gender/${user.gender}`)) {
+      fs.mkdir(__dirname + `/Categories/Gender/${user.gender}`, (err) => {
+        if (err) throw err;
+        console.log('Folder created.')
+      })
+    }
+    else {
+      console.log('Folder already exists.')
+    }
   })
 })
 .catch(console.error)
